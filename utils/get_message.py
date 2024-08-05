e618d482105d1ea09ff778c80f2368e08d1b01d4
Rubén Ortiz <runerd.menethil@gmail.com>
2024-08-05 03:12:50 -0400

    Agregar parametro predeterminado para los canales

--- utils/get_message.py
+++ utils/get_message.py
@@ -1,7 +1,7 @@
 from settings import COMMAND_CHANNEL_ID
 
-async def get_message(bot, message_id):
-    channel = bot.get_channel(COMMAND_CHANNEL_ID)
+async def get_message(bot, message_id, channel_id=COMMAND_CHANNEL_ID):
+    channel = bot.get_channel(channel_id)
     message = await channel.fetch_message(message_id)
 
     return message

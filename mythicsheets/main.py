47d4747897aceae535cb921479a17ae87605b432
Roykovich <runerd.menethil@gmail.com>
2024-09-18 04:35:11 -0300

    fix

--- mythicsheets/main.py
+++ mythicsheets/main.py
@@ -5,7 +5,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 creds = None
 # The file token.json stores the user's access and refresh tokens, and is
@@ -19,7 +19,7 @@ if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file("/sheets/credentials.json", SCOPES)
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
         creds = flow.run_local_server(port=0)
         
     # Save the credentials for the next run
